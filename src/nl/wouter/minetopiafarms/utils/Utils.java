59df527ae00e7c228067334ed0bcc2e1041b0427
MrWouterNL <wouter@minetopiasdb.nl>
2019-03-18 21:40:59 +0100

    Fixed is113orUp() method to fix errors that 1.12 users had.

--- src/nl/wouter/minetopiafarms/utils/Utils.java
+++ src/nl/wouter/minetopiafarms/utils/Utils.java
@@ -50,7 +50,7 @@ public class Utils {
 		String nmsver = Bukkit.getServer().getClass().getPackage().getName();
 		nmsver = nmsver.substring(nmsver.lastIndexOf(".") + 1);
 		return !nmsver.startsWith("v1_7_") && !nmsver.startsWith("v1_8_") && !nmsver.startsWith("v1_9_")
-				&& !nmsver.startsWith("v1_10_") && !nmsver.startsWith("v1_11_");
+				&& !nmsver.startsWith("v1_10_") && !nmsver.startsWith("v1_11_") && !nmsver.startsWith("v1_12_");
 	}
 
 	public static Material getCropsMaterial() {
