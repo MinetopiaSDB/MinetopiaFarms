04bec9383c0b29ad2e1615054529ea58f6f92a87
MrWouterNL <mail@mrwouter.nl>
2020-07-15 20:49:53 +0200

    Change mine items to ores

--- src/main/java/nl/mrwouter/minetopiafarms/events/BlockBreaker.java
+++ src/main/java/nl/mrwouter/minetopiafarms/events/BlockBreaker.java
@@ -17,75 +17,76 @@ import nl.mrwouter.minetopiafarms.utils.Utils;
 
 public class BlockBreaker implements Listener {
 
-    @EventHandler
-    public void onBreak(BlockBreakEvent e) {
-        Player p = e.getPlayer();
-        if (CustomFlags.hasFlag(p, e.getBlock().getLocation())
-                && (!p.hasPermission("minetopiafarms.bypassregions") && p.getGameMode() != GameMode.CREATIVE)) {
-            e.setCancelled(true);
-        }
+	@EventHandler
+	public void onBreak(BlockBreakEvent e) {
+		Player p = e.getPlayer();
+		if (CustomFlags.hasFlag(p, e.getBlock().getLocation())
+				&& (!p.hasPermission("minetopiafarms.bypassregions") && p.getGameMode() != GameMode.CREATIVE)) {
+			e.setCancelled(true);
+		}
 
-        if (e.getBlock().getType().toString().contains("_ORE") && CustomFlags.hasFlag(p, e.getBlock().getLocation())) {
-        	if (p.getGameMode() == GameMode.CREATIVE) {
-                p.sendMessage(Main.getMessage("Creative"));
-                return;
-            }
+		if (e.getBlock().getType().toString().contains("_ORE") && CustomFlags.hasFlag(p, e.getBlock().getLocation())) {
+			if (p.getGameMode() == GameMode.CREATIVE) {
+				p.sendMessage(Main.getMessage("Creative"));
+				return;
+			}
 
-            if (!PlayerManager.getOnlinePlayer(e.getPlayer().getUniqueId()).getPrefix().equalsIgnoreCase("Mijnwerker")) {
-                e.getPlayer().sendMessage(Main.getMessage("BeroepNodig").replaceAll("<Beroep>", "mijnwerker"));
-                e.setCancelled(true);
-                return;
-            }
-            if (!e.getPlayer().getInventory().getItemInMainHand().getType().toString().contains("PICKAXE")) {
-                e.getPlayer().sendMessage(Main.getMessage("ToolNodig").replaceAll("<Tool>", "houweel"));
-                e.setCancelled(true);
-                return;
-            }
-            if (!CustomFlags.isAllowed(p, e.getBlock().getLocation(), "mijn")) {
-                p.sendMessage(Main.getMessage("GeenRegion").replaceAll("<Tag>", "mijn"));
-                e.setCancelled(true);
-                return;
-            }
-            
+			if (!PlayerManager.getOnlinePlayer(e.getPlayer().getUniqueId()).getPrefix()
+					.equalsIgnoreCase("Mijnwerker")) {
+				e.getPlayer().sendMessage(Main.getMessage("BeroepNodig").replaceAll("<Beroep>", "mijnwerker"));
+				e.setCancelled(true);
+				return;
+			}
+			if (!e.getPlayer().getInventory().getItemInMainHand().getType().toString().contains("PICKAXE")) {
+				e.getPlayer().sendMessage(Main.getMessage("ToolNodig").replaceAll("<Tool>", "houweel"));
+				e.setCancelled(true);
+				return;
+			}
+			if (!CustomFlags.isAllowed(p, e.getBlock().getLocation(), "mijn")) {
+				p.sendMessage(Main.getMessage("GeenRegion").replaceAll("<Tag>", "mijn"));
+				e.setCancelled(true);
+				return;
+			}
 
-            Material blockType = e.getBlock().getType().toString().contains("REDSTONE_ORE") ? Material.REDSTONE_ORE : e.getBlock().getType();
-            e.setCancelled(true);
+			Material blockType = e.getBlock().getType().toString().contains("REDSTONE_ORE") ? Material.REDSTONE_ORE
+					: e.getBlock().getType();
+			e.setCancelled(true);
 
-            switch (blockType) {
-                case COAL_ORE:
-                    e.getPlayer().getInventory().addItem(XMaterial.COAL.parseItem());
-                    break;
-                case DIAMOND_ORE:
-                    e.getPlayer().getInventory().addItem(XMaterial.DIAMOND.parseItem());
-                    break;
-                case EMERALD_ORE:
-                    e.getPlayer().getInventory().addItem(XMaterial.EMERALD.parseItem());
-                    break;
-                case GOLD_ORE:
-                    e.getPlayer().getInventory().addItem(XMaterial.GOLD_INGOT.parseItem());
-                    break;
-                case IRON_ORE:
-                    e.getPlayer().getInventory().addItem(XMaterial.IRON_INGOT.parseItem());
-                    break;
-                case LAPIS_ORE:
-                    e.getPlayer().getInventory().addItem(XMaterial.LAPIS_LAZULI.parseItem());
-                    break;
-                case REDSTONE_ORE:
-                    e.getPlayer().getInventory().addItem(XMaterial.REDSTONE.parseItem());
-                    break;
-                default:
-                    //Not important & should be unreachable
-                    return;
-            }
+			switch (blockType) {
+			case COAL_ORE:
+				e.getPlayer().getInventory().addItem(XMaterial.COAL_ORE.parseItem());
+				break;
+			case DIAMOND_ORE:
+				e.getPlayer().getInventory().addItem(XMaterial.DIAMOND_ORE.parseItem());
+				break;
+			case EMERALD_ORE:
+				e.getPlayer().getInventory().addItem(XMaterial.EMERALD_ORE.parseItem());
+				break;
+			case GOLD_ORE:
+				e.getPlayer().getInventory().addItem(XMaterial.GOLD_ORE.parseItem());
+				break;
+			case IRON_ORE:
+				e.getPlayer().getInventory().addItem(XMaterial.IRON_ORE.parseItem());
+				break;
+			case LAPIS_ORE:
+				e.getPlayer().getInventory().addItem(XMaterial.LAPIS_ORE.parseItem());
+				break;
+			case REDSTONE_ORE:
+				e.getPlayer().getInventory().addItem(XMaterial.REDSTONE_ORE.parseItem());
+				break;
+			default:
+				// Not important & should be unreachable
+				return;
+			}
 
-            e.getBlock().getDrops().clear();
-            Utils.ores.put(e.getBlock().getLocation(), e.getBlock().getType());
-            e.getBlock().getLocation().getBlock().setType(Material.COBBLESTONE);
-            Utils.handleToolDurability(e.getPlayer());
-            Bukkit.getScheduler().runTaskLater(Main.getPlugin(), () -> {
-                e.getBlock().setType(blockType);
-                Utils.ores.remove(e.getBlock().getLocation());
-            }, Main.getPlugin().getConfig().getInt("scheduler.miner." + blockType.name()));
-        }
-    }
+			e.getBlock().getDrops().clear();
+			Utils.ores.put(e.getBlock().getLocation(), e.getBlock().getType());
+			e.getBlock().getLocation().getBlock().setType(Material.COBBLESTONE);
+			Utils.handleToolDurability(e.getPlayer());
+			Bukkit.getScheduler().runTaskLater(Main.getPlugin(), () -> {
+				e.getBlock().setType(blockType);
+				Utils.ores.remove(e.getBlock().getLocation());
+			}, Main.getPlugin().getConfig().getInt("scheduler.miner." + blockType.name()));
+		}
+	}
 }
