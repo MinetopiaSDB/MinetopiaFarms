de66b20ebb97bb51a41301bdcf7d5c1c1ea11f19
MrWouterNL <mail@mrwouter.nl>
2020-09-10 18:00:07 +0200

    Fix bug and change version to 2.10

--- src/main/java/nl/mrwouter/minetopiafarms/events/NPCClickListener.java
+++ src/main/java/nl/mrwouter/minetopiafarms/events/NPCClickListener.java
@@ -23,95 +23,95 @@ public class NPCClickListener implements Listener {
 		NPC clicked = e.getNPC();
 		if (Main.getMessage("NPC.Name").equals(clicked.getName())) {
 
-				double paymentAmount = 0;
-				for (ItemStack item : clicker.getInventory().getContents()) {
-					if (item != null && item.getType() != null) {
-						if (item.getType() == Material.BEETROOT) {
-							clicker.getInventory().removeItem(item);
+			double paymentAmount = 0;
+			for (ItemStack item : clicker.getInventory().getContents()) {
+				if (item != null && item.getType() != null) {
+					if (item.getType() == Material.BEETROOT) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount
 									+ (Main.getPlugin().getConfig().getDouble("TerugverkoopPrijs.Boer.BEETROOTS")
 											* item.getAmount());
-						} else if (item.getType() == XMaterial.WHEAT.parseMaterial()) {
-							clicker.getInventory().removeItem(item);
+					} else if (item.getType() == XMaterial.WHEAT.parseMaterial()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount
 									+ (Main.getPlugin().getConfig().getDouble("TerugverkoopPrijs.Boer.WHEAT")
 											* item.getAmount());
-						} else if (item.getType() == Utils.getMelonMaterial()) {
-							clicker.getInventory().removeItem(item);
+					} else if (item.getType() == Utils.getMelonMaterial()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount
 									+ (Main.getPlugin().getConfig().getDouble("TerugverkoopPrijs.Boer.MELON")
 											* item.getAmount());
-						} else if (item.getType() == XMaterial.PUMPKIN.parseMaterial()) {
-							clicker.getInventory().removeItem(item);
+					} else if (item.getType() == XMaterial.PUMPKIN.parseMaterial()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount
 									+ (Main.getPlugin().getConfig().getDouble("TerugverkoopPrijs.Boer.PUMPKIN")
 											* item.getAmount());
-						} else if (item.getType() == Utils.getCarrotItem()) {
-							clicker.getInventory().removeItem(item);
+					} else if (item.getType() == Utils.getCarrotItem()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount
 									+ (Main.getPlugin().getConfig().getDouble("TerugverkoopPrijs.Boer.CARROTS")
 											* item.getAmount());
-						} else if (item.getType() == Utils.getPotatoItem()) {
-							clicker.getInventory().removeItem(item);
+					} else if (item.getType() == Utils.getPotatoItem()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount
 									+ (Main.getPlugin().getConfig().getDouble("TerugverkoopPrijs.Boer.POTATOES")
 											* item.getAmount());
-						}
 					}
 				}
+			}
 
-				for (ItemStack item : clicker.getInventory().getContents()) {
-					if (item != null && item.getType() != null) {
-						if (item.getType() == XMaterial.COAL_ORE.parseMaterial()) {
-							clicker.getInventory().removeItem(item);
+			for (ItemStack item : clicker.getInventory().getContents()) {
+				if (item != null && item.getType() != null) {
+					if (item.getType() == XMaterial.COAL_ORE.parseMaterial()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount
 									+ (Main.getPlugin().getConfig().getDouble("TerugverkoopPrijs.Mijnwerker.COAL_ORE")
 											* item.getAmount());
-						} else if (item.getType() == XMaterial.IRON_ORE.parseMaterial()) {
-							clicker.getInventory().removeItem(item);
+					} else if (item.getType() == XMaterial.IRON_ORE.parseMaterial()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount
 									+ (Main.getPlugin().getConfig().getDouble("TerugverkoopPrijs.Mijnwerker.IRON_ORE")
 											* item.getAmount());
-						} else if (item.getType() == XMaterial.EMERALD_ORE.parseMaterial()) {
-							clicker.getInventory().removeItem(item);
+					} else if (item.getType() == XMaterial.EMERALD_ORE.parseMaterial()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount + (Main.getPlugin().getConfig()
 									.getDouble("TerugverkoopPrijs.Mijnwerker.EMERALD_ORE") * item.getAmount());
-						} else if (item.getType() == XMaterial.GOLD_ORE.parseMaterial()) {
-							clicker.getInventory().removeItem(item);
+					} else if (item.getType() == XMaterial.GOLD_ORE.parseMaterial()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount
 									+ (Main.getPlugin().getConfig().getDouble("TerugverkoopPrijs.Mijnwerker.GOLD_ORE")
 											* item.getAmount());
-						} else if (item.getType() == XMaterial.LAPIS_ORE.parseMaterial()) {
-							clicker.getInventory().removeItem(item);
+					} else if (item.getType() == XMaterial.LAPIS_ORE.parseMaterial()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount
 									+ (Main.getPlugin().getConfig().getDouble("TerugverkoopPrijs.Mijnwerker.LAPIS_ORE")
 											* item.getAmount());
-						} else if (item.getType() == XMaterial.REDSTONE_ORE.parseMaterial()) {
-							clicker.getInventory().removeItem(item);
+					} else if (item.getType() == XMaterial.REDSTONE_ORE.parseMaterial()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount + (Main.getPlugin().getConfig()
 									.getDouble("TerugverkoopPrijs.Mijnwerker.REDSTONE_ORE") * item.getAmount());
-						} else if (item.getType() == XMaterial.DIAMOND_ORE.parseMaterial()) {
-							clicker.getInventory().removeItem(item);
+					} else if (item.getType() == XMaterial.DIAMOND_ORE.parseMaterial()) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount + (Main.getPlugin().getConfig()
 									.getDouble("TerugverkoopPrijs.Mijnwerker.DIAMOND_ORE") * item.getAmount());
-						}
 					}
 				}
+			}
 
-				for (ItemStack item : clicker.getInventory().getContents()) {
-					if (item != null && item.getType() != null) {
-						if (item.getType().toString().contains("LOG")) {
-							clicker.getInventory().removeItem(item);
+			for (ItemStack item : clicker.getInventory().getContents()) {
+				if (item != null && item.getType() != null) {
+					if (item.getType().toString().contains("LOG")) {
+						if (clicker.getInventory().removeItem(item).size() == 0)
 							paymentAmount = paymentAmount + (item.getAmount()
 									* Main.getPlugin().getConfig().getInt("TerugverkoopPrijs.Houthakker"));
-						}
 					}
 				}
+			}
 
-				API.getEcon().depositPlayer(clicker, paymentAmount);
-				clicker.sendMessage(
-						Main.getMessage("GeldBetaald").replaceAll("<Bedrag>", Utils.formatMoney(paymentAmount)));
-				API.updateScoreboard(clicker);
+			API.getEcon().depositPlayer(clicker, paymentAmount);
+			clicker.sendMessage(
+					Main.getMessage("GeldBetaald").replaceAll("<Bedrag>", Utils.formatMoney(paymentAmount)));
+			API.updateScoreboard(clicker);
 
 		}
 	}
